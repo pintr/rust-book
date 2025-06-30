88d4a4de40bf7c9c3709acbb9a9a421109255c97
pintr <brigadoi.michele@gmail.com>
2025-06-30 18:37:06 +0200

    Added traits for inheritance.

--- c18_object_oriented_programming/src/lib.rs
+++ c18_object_oriented_programming/src/lib.rs
@@ -103,3 +103,76 @@ impl AveragedCollection {
         self.average = total as f64 / self.list.len() as f64;
     }
 }
+
+pub mod gui {
+    //! # Gui
+    //!
+    //! A library to draw components using trait objects for dynamic dispatch.
+    //!
+    //! This module provides the `Draw` trait for drawable UI components and the `Screen` struct to manage and render a collection of such components.
+
+    /// A trait for drawable UI components.
+    ///
+    /// Types implementing this trait can be drawn onto a screen.
+    pub trait Draw {
+        /// Draws the component.
+        fn draw(&self);
+    }
+
+    /// A container for drawable components.
+    ///
+    /// The `Screen` struct holds a list of components implementing the `Draw` trait, and can render all of them by calling their `draw` methods.
+    pub struct Screen {
+        /// The list of components to be drawn.
+        pub components: Vec<Box<dyn Draw>>,
+    }
+
+    impl Screen {
+        /// Runs the screen by drawing each component in order.
+        ///
+        /// Iterates over all components and calls their `draw` method.
+        pub fn run(&self) {
+            for component in self.components.iter() {
+                component.draw();
+            }
+        }
+    }
+
+    /// A button component that can be drawn on the screen.
+    ///
+    /// The `Button` struct represents a UI button with a specified width, height, and label.
+    /// It implements the `Draw` trait, allowing it to be rendered as part of a `Screen`.
+    ///
+    /// # Fields
+    ///
+    /// * `width` - The width of the button in pixels.
+    /// * `height` - The height of the button in pixels.
+    /// * `label` - The text label displayed on the button.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use your_crate::gui::{Button, Draw};
+    ///
+    /// let button = Button {
+    ///     width: 100,
+    ///     height: 50,
+    ///     label: String::from("Click me"),
+    /// };
+    /// button.draw();
+    /// ```
+    pub struct Button {
+        pub width: u32,
+        pub height: u32,
+        pub label: String,
+    }
+
+    impl Draw for Button {
+        /// Draws the button component.
+        ///
+        /// This method is called when rendering the button as part of a `Screen`.
+        fn draw(&self) {
+            // Draw the button
+        }
+    }
+}
--- c18_object_oriented_programming/src/main.rs
+++ c18_object_oriented_programming/src/main.rs
@@ -3,11 +3,10 @@
 //! The procedures are typically called methods or operations
 //! Rust has some characteristics commonly considered object oriented: strcts and enums have data, and `impl` blocks provide methods.
 
-use c18_object_oriented_programming::AveragedCollection;
-
 fn main() {
     encapsulation_inheritance();
     traits_for_inheritance();
+    object_oriented_design_pattern();
 }
 
 fn encapsulation_inheritance() {
@@ -25,6 +24,8 @@ fn encapsulation_inheritance() {
         // When an item is added or removed from the list, each function calls the private `update_average` that handles the updating of the `average` as well.
         // The `list` and `average` fields are private so there is no way to update the items from the fields directly, otherwise `average` would go out of sync.
         // The `average` method return the `average` field value.
+        use c18_object_oriented_programming::AveragedCollection;
+
         let mut collection = AveragedCollection::new();
 
         println!("Add 10 to the collection");
@@ -60,4 +61,103 @@ fn encapsulation_inheritance() {
     }
 }
 
-fn traits_for_inheritance() {}
+fn traits_for_inheritance() {
+    // In chapter 8 an enum was defined to store different value types in a vector.
+    // However it may be useful to let user extend the set of types that are valid
+    // An example is a GUI tool, called `gui`, that lets the user draw GUI tools, some tools may be `Button` or `TextField` but other can be added too, such as `Image` or `SelectBox`
+    // The `gui` library needs to keep track of the values of different types it needs to call the `draw` method on each of these typed values.
+    // It's not important what the `draw` function does, just that the value has the method available.
+    // In a language with inheritance this could be done by defining a class `Component` that has a method `draw` on it.
+    // The other classes, such as `Button`, would inherit from `Component` and thus inherit the `draw` method too, that could be overriden to define a custom behaviour.
+    // Since Rust doesn't have inheritance traits are used to allow users to extend the `gui` library.
+    // To implement this behaviour a trait called `Draw` with a method `draw` is needed, then it is possible to define a vactor that takes a trait object.
+    // A trait object points to both an instance of a type implementing a trait, and a table to llok up trait methods on that type at runtime.
+    // A trait object can be created by specifying some sort of pointer, such as `&` or `Box<T>`, then the `dyn` keyword, and specifying the trait.
+    // A trait object can be used in place of a generic or concrete type and Rust will ensure at compile time that any value used in the context implements that trait, without knowing all the possible types.
+    // Trait objects, differently from structs or enum that require `impl to specify a behaviour, combine data and behaviour.
+    // The purpose of a trait object is to allow abstraction across common behaviour
+    // So, the `gui` module needs a trait `Draw` with a method `draw`, and a struct `Screen` that holds a vector called components of type `Box<dyn Draw>`
+    // `Box<dyn Draw>` is a trait object and allows to insert any element that implement the `Draw` trait
+    // On the `Screen` struct it's defined a method `run` that calls the `draw` method on each of the `components`
+    // This is different from defining a struct that uses a generic type parameter with trait bounds, because it can only be substituted with one concrete type at a time, not multiple
+
+    {
+        // An example of this implementation would be the following:
+        use c18_object_oriented_programming::gui::Draw;
+        pub struct _Screen<T: Draw> {
+            pub components: Vec<T>,
+        }
+
+        impl<T> _Screen<T>
+        where
+            T: Draw,
+        {
+            pub fn _run(&self) {
+                for component in self.components.iter() {
+                    component.draw();
+                }
+            }
+        }
+        // This restricts the `Screen` instance to use a list of components all of the same type (such as `Button`, or `TextField`)
+        // Anyway, with an homogeneous collection, using generics and trait bounds is preferable because the definitions are monomorphised at compile time.
+        // On the other hand with the method using trait objects can hold a `Vec<T>` that contains `Box<Button>` as well as `Box<TextField>`
+    }
+    // In the `gui` library a button has been created implementing the `Draw`trait
+    // The fields on Button (`width`, `height`, and `label`) may differ from other components, such as `TextField` which would add a `placeholder`
+    // Each of the types will implement the `Draw` trait, but the method `draw` is different for each of them, they could even have additional `impl` blocks containing methods realted to other events (e.g. click of button)
+    // Here is the implementation of a `SelectBox` using `Draw`:
+
+    use c18_object_oriented_programming::gui::{Button, Draw, Screen};
+
+    {
+        #[allow(dead_code)]
+        struct SelectBox {
+            width: u32,
+            height: u32,
+            options: Vec<String>,
+        }
+
+        impl Draw for SelectBox {
+            fn draw(&self) {
+                // Draw the select box
+            }
+        }
+        // Here is the `Screen` instance used for adding the components and draw the using the `run` function, which will call the `draw` method of each component:
+        let screen = Screen {
+            components: vec![
+                Box::new(SelectBox {
+                    width: 75,
+                    height: 10,
+                    options: vec![
+                        String::from("Yes"),
+                        String::from("Maybe"),
+                        String::from("No"),
+                    ],
+                }),
+                Box::new(Button {
+                    width: 50,
+                    height: 10,
+                    label: String::from("OK"),
+                }),
+            ],
+        };
+
+        screen.run();
+    }
+    // When the `gui` library was written the added components aren't known, such as `SelectBox`, but the `Screen` implementation allows it since it works with the `Draw` trait.
+    // Similarly, when `screen.run()` is called it doesn't need to know what the concrete type of each component is, it just calls the `draw` method,which is present as specified by the `Box<dyn Draw>` type.
+    // For example adding another element that doesn't implement it, results in an error:
+    // let screen = Screen {
+    //     components: vec![Box::new(String::from("Hi"))],
+    // };
+    // screen.run();
+    // The error is the following: the trait `Draw` is not implemented for `String`
+    // The compiler generates nongeneric implementations of functions and methods for each concrete type used in place of a generic type parameter.
+    // The code that results from monomorphisation does static dispatch: the compiler knows the method called at compile time
+    // In this case, instead, dynamic dispatch is used: the compiler doesn't know at compile time which concrete method is called, the compiler emits code that it is figured out at runtime.
+    // At compile time the compiler doesn't know all the types that might be used, so the method to call, instead at runtime Rust uses the pointer inside the trait object to know which method to call.
+    // Dynamic dispatch prevents prvents the compiler from choosing to inline a method's code, which prevents some optimisation, and Rust has rules called syn compatibility, about where dynamic dispatch can be used.
+    // However this introduces flexibility, but the trade-off is to consider.
+}
+
+fn object_oriented_design_pattern() {}
